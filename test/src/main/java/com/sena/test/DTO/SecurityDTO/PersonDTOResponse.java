f098ac927e0e3bedf3d0d115cdfa541117c6acf1
Karen <klddaniela8@gmail.com>
2026-05-11 16:03:55 -0500

    create domain tables inventory to main

---
+++ test/src/main/java/com/sena/test/DTO/SecurityDTO/PersonDTOResponse.java
@@ -0,0 +1,14 @@
+package com.sena.test.DTO.SecurityDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonDTOResponse {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
